6e00a4f058fe60a92c45963b61ebee90fd630b7e
ylu <123523291+ylu8888@users.noreply.github.com>
2023-11-07 13:05:29 -0500

    Create main.c

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+
+#include "deet.h"
+
+int main(int argc, char *argv[]) {
+    // TO BE IMPLEMENTED
+    // Remember: Do not put any functions other than main() in this file.
+    abort();
+}
