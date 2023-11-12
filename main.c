4410bdb4eec0444df8b8f9a5281ef150827dd39d
ylu <123523291+ylu8888@users.noreply.github.com>
2023-11-12 18:51:07 -0500

    initial progress

--- main.c
+++ main.c
@@ -3,7 +3,18 @@
 #include "deet.h"
 
 int main(int argc, char *argv[]) {
-    // TO BE IMPLEMENTED
-    // Remember: Do not put any functions other than main() in this file.
-    abort();
+
+    void log_startup(void); //starts up deets
+
+    void log_prompt(void); // issues a prompt
+
+    fprintf(stderr, "deet> ");
+
+    void log_input(char *line); // read a line of input from user
+
+
+    
+    
+    return 0;
+    
 }
