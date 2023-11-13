2feeba66dd89798815a880dc58012c9ab934e798
ylu <123523291+ylu8888@users.noreply.github.com>
2023-11-12 19:14:39 -0500

    fin buffer and input

--- main.c
+++ main.c
@@ -1,20 +1,26 @@
 #include <stdlib.h>
-
+#include <stdio.h>
 #include "deet.h"
 
 int main(int argc, char *argv[]) {
 
-    void log_startup(void); //starts up deets
+    log_startup(); //starts up deets
 
-    void log_prompt(void); // issues a prompt
+    log_prompt(); // issues a prompt
 
-    fprintf(stderr, "deet> ");
+    fprintf(stdout, "deet> ");
 
-    void log_input(char *line); // read a line of input from user
+    char* buffer = NULL; //store the user input into a buffer
+    size_t buffSize = 0;
+
+    getline(&buffer,&buffSize,stdin);   //use get line to get user input
+
+    log_input(buffer); // read a line of input from user
+
+    log_prompt(); // issues another prompt
+
+    fflush(stdout); //fflush the stdout after every time the user prints something
 
 
-    
-    
     return 0;
-    
 }
